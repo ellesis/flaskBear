142f741724835b6c1990479d33c915938b69b00b
Lisa Sunhwa Han <ellesis@gmail.com>
2017-02-02 15:49:17 -0800

    saved data

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ from flask import (Flask, render_template, redirect,
 
 app = Flask(__name__)
 
+def get_saved_data():
+  try:
+    data = json.loads(request.cookies.get('character'))
+  except TypeError:
+    data = {}
+  return data
+
 @app.route('/')
 def index(): 
-  return render_template("index.html")
+  data = get_saved_data()
+  return render_template("index.html", saves=data)
 
 @app.route('/save', methods=['post'])
 def save():
   response = make_response(redirect(url_for('index')))
-  response.set_cookie('character', json.dumps(dict(request.form.items())))
+  data = get_saved_data()
+  data.update(dict(request.form.items()))
+  response.set_cookie('character', json.dumps(data))
   return response
-
-
-
 
 app.run(debug=True, port=8000, host='0.0.0.0')
